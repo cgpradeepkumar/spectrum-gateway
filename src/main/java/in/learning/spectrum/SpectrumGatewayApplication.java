07bdaab4f153bf28bf307b2844b48c929bf9413c
pradeepkumar <cgpradeepkumar@gmail.com>
2025-10-18 22:12:13 +0100

    initial commit

---
+++ src/main/java/in/learning/spectrum/SpectrumGatewayApplication.java
@@ -0,0 +1,13 @@
+package in.learning.spectrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectrumGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpectrumGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/learning/spectrum/SpectrumGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package in.learning.spectrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpectrumGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
