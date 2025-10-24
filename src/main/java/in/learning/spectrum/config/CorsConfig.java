1f693325e3c2004cf3779fbb196acc36e7b2bb07
pradeepkumar <cgpradeepkumar@gmail.com>
2025-10-25 00:16:27 +0100

    cors config

---
+++ src/main/java/in/learning/spectrum/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package in.learning.spectrum.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                .allowedHeaders("*"); // Allowed headers
+    }
+
+}
